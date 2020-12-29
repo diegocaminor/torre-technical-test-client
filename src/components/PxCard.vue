48ed04b177dd070c63c0ec87cbd2fca3de98973c
Diego Camino <diego.camino.r@gmail.com>
2020-12-29 03:16:35 -0500

    Add Card component

---
+++ src/components/PxCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-card
+        :title="title"
+        :img-src="imgSrc"
+        :img-alt="imgAlt"
+        img-height="200"
+        img-width="200"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>
+          Job position: {{ jobPosition }}
+          Type: {{ jobType }}
+        </b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PxCard",
+  props: {
+    title: String,
+    imgSrc: String,
+    imgAlt: String,
+    jobPosition: String,
+    jobType: String,
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="home">
+  <div class="home mt-5">
     <h1>Matchify</h1>
     <h3>By Torre</h3>
     <p>Matchify your next remote dream job!</p>
     <p>You just have to write your Torre username and our system will take care of showing you the best positions that fit your skills</p>
     <input v-on:keyup="pressKey" type="text" id="username" name="username" v-model="username"><br><br>
-    <button @click='getJobsBySkills'>Search!</button>
-    
-    <ul id="example-1">
-      <li v-for="job in matchedJobs" :key="job.id">
-        {{ job.objective }}
-      </li>
-    </ul>
+    <b-button variant="primary" @click='getJobsBySkills'>Search!</b-button>
+
+    <b-row class="mt-5">
+      <b-col cols="12" md="6" lg="4" v-for="job in matchedJobs" :key="job.id">
+        <px-card
+          :title ="job.organizations[0].name"
+          :imgSrc ="job.organizations[0].picture"
+          :imgAlt ="job.organizations[0].name"
+          :jobPosition ="job.objective"
+          :jobType ="job.type"
+        />
+      </b-col>
+    </b-row>
+
     <h1 v-if="existErrorMessage">{{errorMessage}}</h1>
 
   </div>
@@ -20,10 +27,12 @@
 <script>
 // @ is an alias to /src
 import { mapState } from 'vuex'
+import PxCard from "@/components/PxCard.vue";
 
 export default {
   name: "Home",
   components: {
+    PxCard
   },
   data() {
     return {
